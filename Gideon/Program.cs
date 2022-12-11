b4d4633ede9395caeaf75f97a91e03f1808cb4fb
ividyon <shrinkray@gmail.com>
2022-12-11 08:47:46 +0100

    Initial commit.

---
+++ Gideon/Program.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using SoulsFormats;
+
+namespace Gideon
+{
+    internal class Program
+    {
+        private enum RunMode
+        {
+            Prompts,
+            ConsolePrompts,
+            Arguments
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            RunMode mode = RunMode.Prompts;
+
+            string ddsPath = null;
+            string bhdPath = null;
+
+            if (args.Contains("-c") || args.Contains("--console"))
+            {
+                mode = RunMode.ConsolePrompts;
+            }
+            else if (args.Length == 2)
+            {
+                mode = RunMode.Arguments;
+            }
+
+            if (mode != RunMode.Prompts)
+            {
+                Console.WriteLine("You've received the wisdom of the Two Fingers, have you not?");
+                Console.WriteLine("I bid you welcome, as a true member of the Roundtable.");
+                Console.WriteLine("I am known as Gideon Ofnir.");
+            }
+
+            switch (mode)
+            {
+                case RunMode.Prompts:
+                    using (var dirDialog = new CommonOpenFileDialog())
+                    {
+                        dirDialog.IsFolderPicker = true;
+                        dirDialog.Title = "Select folder containing MENU_Knowledge DDS files...";
+                        CommonFileDialogResult result = dirDialog.ShowDialog();
+
+                        if (result == CommonFileDialogResult.Ok && !string.IsNullOrWhiteSpace(dirDialog.FileName))
+                        {
+                            ddsPath = dirDialog.FileName;
+                        }
+                        else if (result == CommonFileDialogResult.Cancel)
+                        {
+                            return;
+                        }
+                    }
+
+                    using (var fileDialog = new OpenFileDialog())
+                    {
+                        fileDialog.Title = "Select TPFBHD file...";
+                        fileDialog.Filter = "TPFBHD files (*.tpfbhd)|*.tpfbhd|All files (*.*)|*.*";
+                        fileDialog.Multiselect = false;
+
+                        DialogResult result = fileDialog.ShowDialog();
+
+                        if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fileDialog.FileName))
+                        {
+                            bhdPath = fileDialog.FileName;
+                        }
+                        else if (result == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+                    }
+
+                    break;
+                case RunMode.ConsolePrompts:
+                    Console.Write("Enter path to folder containing DDS files:");
+                    ddsPath = Console.ReadLine();
+                    Console.Write("Enter path to DCX file to embed the DDS files in:");
+                    bhdPath = Console.ReadLine();
+                    break;
+                case RunMode.Arguments:
+                    ddsPath = args[0];
+                    bhdPath = args[1];
+                    break;
+            }
+
+            if (ddsPath == null || !Directory.Exists(ddsPath))
+            {
+                switch (mode)
+                {
+                    case RunMode.Prompts:
+                        MessageBox.Show(
+                            "Please provide a valid path to a folder containing DDS files with the right naming (MENU_Knowledge_*.dds).",
+                            "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    default:
+                        Console.Write(
+                            "Please provide a valid path to a folder containing DDS files with the right naming (MENU_Knowledge_*.dds).");
+                        break;
+                }
+
+                return;
+            }
+
+            if (bhdPath == null || !File.Exists(bhdPath))
+            {
+                switch (mode)
+                {
+                    case RunMode.Prompts:
+                        MessageBox.Show("Please provide a valid path to a TPFBHD archive file.", "Error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        break;
+                    default:
+                        Console.Write("Please provide a valid path to a TPFBHD archive file.");
+                        break;
+                }
+
+                return;
+            }
+
+            string[] ddsFilePaths = Directory.GetFiles(ddsPath, "MENU_Knowledge_*.dds");
+
+            string bdtPath = $@"{Path.GetDirectoryName(bhdPath)}\{Path.GetFileNameWithoutExtension(bhdPath)}.tpfbdt";
+            BXF4 bxf = BXF4.Read(bhdPath, bdtPath);
+            int nextId = bxf.Files.OrderByDescending(item => item.ID).First().ID + 1;
+            int fileWrites = 0;
+            int replaces = 0;
+            int newFiles = 0;
+            foreach (string ddsFilePath in ddsFilePaths)
+            {
+                string textureName = Path.GetFileNameWithoutExtension(ddsFilePath);
+                byte[] dds = File.ReadAllBytes(ddsFilePath);
+                TPF tpf = new TPF();
+                TPF.Texture texture = new TPF.Texture();
+                texture.Name = textureName;
+                texture.Bytes = dds;
+                tpf.Textures.Add(texture);
+                byte[] dcx = tpf.Write(DCX.Type.DCX_KRAK);
+
+                BinderFile existingFile = bxf.Files.Find(file => file.Name == $@"00_Solo\{textureName}.tpf.dcx");
+                if (existingFile != null)
+                {
+                    existingFile.Bytes = dcx;
+                    replaces++;
+                }
+                else
+                {
+                    bxf.Files.Add(
+                        new BinderFile(Binder.FileFlags.Flag1, nextId, $@"00_Solo\{textureName}.tpf.dcx", dcx));
+                    newFiles++;
+                }
+
+                nextId++;
+                fileWrites++;
+            }
+
+            bxf.Write(bhdPath, bdtPath);
+
+            string message =
+                $"Successfully wrote {fileWrites} files ({newFiles} new files, {replaces} existing files replaced)";
+
+            switch (mode)
+            {
+                case RunMode.Prompts:
+                    MessageBox.Show(message, "You are a true fellow", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1, MessageBoxOptions.DefaultDesktopOnly);
+                    break;
+                default:
+                    Console.WriteLine(message);
+                    break;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,42 @@
+﻿# Gideon
+
+Gideon Ofnir, the All-Knowing, is here to carefully handle your MENU_Knowledge files for you. (Tool for saving high-resolution icons in ELDEN RING modding)
+
+**This is only for ELDEN RING**, not the other franchise games. It could technically work in other games, but I'd have to make that happen first.
+
+You might need the Oodle DLL from your game folder, or perhaps not, I'm not sure.
+
+## What's this for?
+
+Adding new icons for new items to [ELDEN RING Reforged](https://www.nexusmods.com/eldenring/mods/541), I noticed that this is done with the following obnoxious workflow:
+
+* Extract `00_Solo.tpfbhd`.
+* Extract one of the `MENU_Knowledge_XXX.tpf.dcx` archives within, using Yabber.
+* Replace the DDS within the extracted TPF archive with yours.
+* Change the Yabber index file to reflect the new filename.
+* Re-pack the newly made TPF.
+* Repeat for EVERY SINGLE ICON!!!
+* Re-pack `00_Solo.tpfbhd`.
+
+It is enough to drive a man insane. No more!
+
+## Usage
+
+Point Gideon at a folder containing DDS files named in the required `MENU_Knowledge_XXX.dds` pattern, and your `00_Solo.tpfbhd` file, and watch him go!
+
+There are three ways to run the tool:
+
+* Run `Gideon.exe` directly from Windows. You will receive user-friendly prompts for the paths.
+* Run `Gideon.exe` with the argument `-c` or `--console` to receive prompts for the paths.
+* Run `Gideon.exe` with two arguments, the first being the path to your DDS files, the second being the path to the TPFBHD file.
+
+The file will replace existing files with their new versions, and add new files automagically. No additional work required.
+
+## Disclaimer
+
+I'm fairly new at C# and Souls Modding, so there are no guarantees for perfect function. Write me on Discord or write an issue here if something's off.
+
+## Credits
+
+ * Thank you to [TKGP](https://github.com/JKAnderson) for his [SoulsFormats library](https://github.com/JKAnderson/SoulsFormats) which is the only reason this even exists.
+ * Kirnifr for [ELDEN RING Reforged](https://www.nexusmods.com/eldenring/mods/541) and the privilege of working on it
